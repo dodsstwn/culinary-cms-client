87ff7ad05091447b6bb15b459140805982d87e9b
dodsstwn <52574922+dodsstwn@users.noreply.github.com>
2021-04-22 06:01:39 +0700

    Add axios config

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  // baseURL: 'https://dodsstwn-gadget-server.herokuapp.com/'
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://culinary-cms.herokuapp.com/'
+  // baseURL: 'http://localhost:3000'
 })
 
 export default instance
